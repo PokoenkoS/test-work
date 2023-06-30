fd9ea70ef33309bae39ae4aa0a2d6fa0e8383b76
PokoenkoS <pokoenkosveta@gmail.com>
2023-06-30 15:49:43 +0300

    Updates

--- static/js/630.0405aa7a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktest_work=self.webpackChunktest_work||[]).push([[630],{424:function(e,t,n){n.r(t),n.d(t,{default:function(){return a}});var s=n(96),r=n(539),c=n.p+"static/media/Boy.e70bc2802efa1b14eda5.png",u=n(184),a=function(){return(0,u.jsxs)(r.K,{children:[(0,u.jsx)(s.Dx,{children:"WELCOME TO TWEETS!"}),(0,u.jsx)(s.P_,{src:c,alt:"boy"})]})}}}]);
-//# sourceMappingURL=630.0405aa7a.chunk.js.map
